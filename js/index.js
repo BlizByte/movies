69a8cb8010319115e4f768ea543491b18c350961
firmansyahken <firmansyahken4@gmail.com>
2024-07-29 12:53:21 +0700

    Making Movies page

--- js/index.js
+++ js/index.js
@@ -1,8 +1,11 @@
-var link = "https://api.themoviedb.org/3/movie/popular?api_key=241213c1fea22c0b49c61e53273561e9"
+var link_popular = "https://api.themoviedb.org/3/movie/popular?api_key=241213c1fea22c0b49c61e53273561e9"
+var link_category = "https://api.themoviedb.org/3/genre/movie/list?api_key=241213c1fea22c0b49c61e53273561e9"
+var link_content = "https://api.themoviedb.org/3/discover/movie?api_key=241213c1fea22c0b49c61e53273561e9&with_genres="
 var gambar = "https://image.tmdb.org/t/p/w300"
 var populer = document.getElementById("film-populer")
 
-fetch(link).then(r => r.json())
+// Fetch Popular Data
+fetch(link_popular).then(r => r.json())
 .then((response) => {
     var hasil = response.results
     hasil.map(film => {
@@ -12,3 +15,40 @@ fetch(link).then(r => r.json())
         </div>`
     })
 })
+
+var input_filter = document.getElementById("filter");
+var button_filter = document.getElementById("button-filter");
+
+// Fetch Category Data
+function getCategory() {
+    fetch(link_category).then(r => r.json())
+    .then((response) => {
+        var genres = response.genres;
+        genres.map(genre => {
+            input_filter.innerHTML += `<option value="${genre.id}">${genre.name}</option>` 
+        })
+    })
+}
+
+var movies = document.getElementById("movies");
+
+// Fetch content by category
+function getContentCategory(id) {
+    fetch(link_content + id).then(r => r.json())
+    .then((response) => {
+        var results = response.results
+        results.map(result => {
+            movies.innerHTML += `<img src="${gambar + result.poster_path}"/>`
+        })
+    })
+}
+
+function tampilkanFilter() {
+    var value_filter = input_filter.value
+    movies.innerHTML = ''
+    getContentCategory(value_filter)
+}
+
+button_filter.addEventListener("click", tampilkanFilter);
+getCategory()
+getContentCategory("28")
